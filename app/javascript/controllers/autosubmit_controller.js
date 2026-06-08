674a4faed57cae96806fb1b6749c39f9b5e176d2
Vinicios Wentz <wentz.vinicios@gmail.com>
2026-06-08 16:55:48 -0300

    debounce:

--- app/javascript/controllers/autosubmit_controller.js
+++ app/javascript/controllers/autosubmit_controller.js
@@ -1,6 +1,10 @@
 import { Controller } from "@hotwired/stimulus"
-
+import debounce from "debounce"
 export default class extends Controller {
+  initialize() {
+    this.submit = debounce(this.submit.bind(this), 300);
+  }
+
   submit() {
     this.element.form.requestSubmit();
   }
